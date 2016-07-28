6aa23b789e19c3538406bf2162d667da815f80c7
Michael Blunck <michael.blunck@phth.de>
2016-07-28 18:11:19 +0200

    [BUGFIX] Fixes accordion

--- typo3conf/ext/angelshop/Resources/Private/Templates/Gridelements/accordion/accordion.ts
+++ typo3conf/ext/angelshop/Resources/Private/Templates/Gridelements/accordion/accordion.ts
@@ -3,8 +3,7 @@ tt_content.gridelements_pi1.20.10.setup.accordion {
         20 {
             renderObj =< tt_content
             wrap = <div class="panel-group" id="accordion-{field:uid}">|</div>
-            allowedGridTypes  = accordionItem
-            allowed = ''
+      
         }
     }
 
