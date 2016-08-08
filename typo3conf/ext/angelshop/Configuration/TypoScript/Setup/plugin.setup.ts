831d8162226b024c5c0331f776ac805c12d5cf07
Michael Blunck <michael.blunck@phth.de>
2016-08-08 20:12:42 +0200

    [TASK] Clean up code

--- typo3conf/ext/angelshop/Configuration/TypoScript/Setup/plugin.setup.ts
+++ typo3conf/ext/angelshop/Configuration/TypoScript/Setup/plugin.setup.ts
@@ -1,58 +1,61 @@
-plugin.tx_angelshop {
-    view {
-        templateRootPaths.0 = {$plugin.tx_angelshop.view.templateRootPath}
-        partialRootPaths.0 = {$plugin.tx_angelshop.view.partialRootPath}
-        layoutRootPaths.0 = {$plugin.tx_angelshop.view.layoutRootPath}
+plugin {
+    tx_angelshop {
+        view {
+            templateRootPaths.0 = {$plugin.tx_angelshop.view.templateRootPath}
+            partialRootPaths.0 = {$plugin.tx_angelshop.view.partialRootPath}
+            layoutRootPaths.0 = {$plugin.tx_angelshop.view.layoutRootPath}
+        }
+
+        persistence {
+            storagePid = {$plugin.tx_angelshop_gallery.persistence.storagePid}
+        }
     }
 
-    persistence {
-        storagePid = {$plugin.tx_angelshop_gallery.persistence.storagePid}
+    tx_product_list {
+        view {
+            templateRootPaths.0 = {$plugin.tx_angelshop.view.templateRootPath}
+            partialRootPaths.0 = {$plugin.tx_angelshop.view.partialRootPath}
+            layoutRootPaths.0 = {$plugin.tx_angelshop.view.layoutRootPath}
+        }
     }
+
+    tx_angelshop_gallery {
+        view {
+            templateRootPaths.0 = {$plugin.tx_angelshop_gallery.view.templateRootPath}
+            partialRootPaths.0 = {$plugin.tx_angelshop_gallery.view.partialRootPath}
+            layoutRootPaths.0 = {$plugin.tx_angelshop_gallery.view.layoutRootPath}
+        }
+
+        persistence {
+            storagePid = {$plugin.tx_angelshop_gallery.persistence.storagePid}
+        }
+    }
+
+    tx_angelshop_teaserrow {
+        view {
+            templateRootPaths.0 = {$plugin.tx_angelshop_teaserrow.view.templateRootPath}
+            partialRootPaths.0 = {$plugin.tx_angelshop_teaserrow.view.partialRootPath}
+            layoutRootPaths.0 = {$plugin.tx_angelshop_teaserrow.view.layoutRootPath}
+        }
+
+        persistence {
+            storagePid = {$plugin.tx_angelshop_teaserrow.persistence.storagePid}
+        }
+    }
+
+    tx_angelshop_fullwidthvideo {
+        view {
+            templateRootPaths.0 = {$plugin.tx_angelshop_fullwidthvideo.view.templateRootPath}
+            partialRootPaths.0 = {$plugin.tx_angelshop_fullwidthvideo.view.partialRootPath}
+            layoutRootPaths.0 = {$plugin.tx_angelshop_fullwidthvideo.view.layoutRootPath}
+        }
+
+        persistence {
+            storagePid = {$plugin.tx_angelshop_fullwidthvideo.persistence.storagePid}
+        }
+    }
+
+    tx_frontend._CSS_DEFAULT_STYLE >
 }
 
-plugin.tx_product_list {
-    view {
-        templateRootPaths.0 = {$plugin.tx_angelshop.view.templateRootPath}
-        partialRootPaths.0 = {$plugin.tx_angelshop.view.partialRootPath}
-        layoutRootPaths.0 = {$plugin.tx_angelshop.view.layoutRootPath}
-    }
-}
-
-plugin.tx_angelshop_gallery {
-    view {
-        templateRootPaths.0 = {$plugin.tx_angelshop_gallery.view.templateRootPath}
-        partialRootPaths.0 = {$plugin.tx_angelshop_gallery.view.partialRootPath}
-        layoutRootPaths.0 = {$plugin.tx_angelshop_gallery.view.layoutRootPath}
-    }
-
-    persistence {
-        storagePid = {$plugin.tx_angelshop_gallery.persistence.storagePid}
-    }
-}
-
-plugin.tx_angelshop_teaserrow {
-    view {
-        templateRootPaths.0 = {$plugin.tx_angelshop_teaserrow.view.templateRootPath}
-        partialRootPaths.0 = {$plugin.tx_angelshop_teaserrow.view.partialRootPath}
-        layoutRootPaths.0 = {$plugin.tx_angelshop_teaserrow.view.layoutRootPath}
-    }
-
-    persistence {
-        storagePid = {$plugin.tx_angelshop_teaserrow.persistence.storagePid}
-    }
-}
-
-plugin.tx_angelshop_fullwidthvideo {
-    view {
-        templateRootPaths.0 = {$plugin.tx_angelshop_fullwidthvideo.view.templateRootPath}
-        partialRootPaths.0 = {$plugin.tx_angelshop_fullwidthvideo.view.partialRootPath}
-        layoutRootPaths.0 = {$plugin.tx_angelshop_fullwidthvideo.view.layoutRootPath}
-    }
-
-    persistence {
-        storagePid = {$plugin.tx_angelshop_fullwidthvideo.persistence.storagePid}
-    }
-}
-
-plugin.tx_frontend._CSS_DEFAULT_STYLE >
 
