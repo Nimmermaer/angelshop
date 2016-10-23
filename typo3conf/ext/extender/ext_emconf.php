52aa5a23eec3b99ec0bf5bee7c95d3a254697613
Michael Blunck <michael.blunck@phth.de>
2016-10-24 00:19:57 +0200

    [TASK] Installs extender

---
+++ typo3conf/ext/extender/ext_emconf.php
@@ -0,0 +1,41 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "extender".
+ *
+ * Auto generated | Identifier: c63bbfb7048a3c2bdc11891c02cb2c0d
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'Extbase Domain Model Extender',
+	'description' => '',
+	'category' => 'misc',
+	'version' => '6.3.1',
+	'state' => 'beta',
+	'uploadfolder' => false,
+	'createDirs' => '',
+	'clearcacheonload' => true,
+	'author' => 'Sebastian Fischer',
+	'author_email' => 'typo3@evoweb.de',
+	'author_company' => 'evoweb',
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
+			'typo3' => '6.2.10-7.9.99',
+		),
+		'conflicts' => 
+		array (
+		),
+		'suggests' => 
+		array (
+		),
+	),
+	'_md5_values_when_last_written' => 'a:23:{s:8:"Classes/";s:4:"d41d";s:16:"Classes/Utility/";s:4:"d41d";s:37:"Classes/Utility/ClassCacheManager.php";s:4:"cf79";s:31:"Classes/Utility/ClassLoader.php";s:4:"297f";s:18:"Classes/Exception/";s:4:"d41d";s:43:"Classes/Exception/FileNotFoundException.php";s:4:"3288";s:13:"composer.json";s:4:"cba1";s:14:"Documentation/";s:4:"d41d";s:27:"Documentation/Installation/";s:4:"d41d";s:36:"Documentation/Installation/Index.rst";s:4:"8076";s:23:"Documentation/Index.rst";s:4:"deed";s:27:"Documentation/Introduction/";s:4:"d41d";s:36:"Documentation/Introduction/Index.rst";s:4:"076d";s:24:"Documentation/Images.txt";s:4:"8410";s:28:"Documentation/Configuration/";s:4:"d41d";s:37:"Documentation/Configuration/Index.rst";s:4:"44bb";s:26:"Documentation/Includes.txt";s:4:"d959";s:26:"Documentation/Settings.yml";s:4:"6ea7";s:21:"Documentation/Images/";s:4:"d41d";s:43:"Documentation/Images/background_devider.gif";s:4:"94bd";s:35:"Documentation/Images/logo_typo3.png";s:4:"f048";s:12:"ext_icon.gif";s:4:"c675";s:17:"ext_localconf.php";s:4:"4717";}',
+);
+
+?>
