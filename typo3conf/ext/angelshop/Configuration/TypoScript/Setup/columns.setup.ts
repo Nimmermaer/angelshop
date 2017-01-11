dd65e0cccabf389c1bca853c6c76a8dcee9ec2b2
Michael Blunck <michael.blunck@phth.de>
2017-01-12 00:22:24 +0100

    [TASK] Adds footer columns

--- typo3conf/ext/angelshop/Configuration/TypoScript/Setup/columns.setup.ts
+++ typo3conf/ext/angelshop/Configuration/TypoScript/Setup/columns.setup.ts
@@ -16,12 +16,26 @@ lib {
                 20.select.where = colPos=1
                 20.wrap < temp.typo3searchWrap
             }
-
-            bottom = COA
-            bottom {
+            footerLeft = COA
+            footerLeft {
                 20 < styles.content.get
                 20.select.where = colPos=2
                 20.wrap < temp.typo3searchWrap
+                20.slide = -1
+            }
+            footerMiddle = COA
+            footerMiddle {
+                20 < styles.content.get
+                20.select.where = colPos=3
+                20.wrap < temp.typo3searchWrap
+                20.slide = -1
+            }
+            footerRight = COA
+            footerRight {
+                20 < styles.content.get
+                20.select.where = colPos=4
+                20.wrap < temp.typo3searchWrap
+                20.slide = -1
             }
         }
     }
