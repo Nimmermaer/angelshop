4e3155c54565b593d4cc155e2d68c9c653b13060
Michael Blunck <michael.blunck@phth.de>
2017-01-12 01:08:25 +0100

    [TASK] Adds news extension

---
+++ typo3conf/ext/angelshop/Classes/Domain/Model/News.php
@@ -0,0 +1,86 @@
+<?php
+namespace MB\Angelshop\Domain\Model;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+
+/**
+ * Class News
+ * @package MB\Angelshop\Domain\Model
+ */
+class News extends \GeorgRinger\News\Domain\Model\News
+{
+    /**
+     * @var bool
+     */
+    protected $recipe;
+
+    /**
+     * @var string
+     */
+    protected $icon;
+
+    /**
+     * @var string
+     */
+    protected $ingredient;
+
+    /**
+     * @return boolean
+     */
+    public function isRecipe()
+    {
+        return $this->recipe;
+    }
+
+    /**
+     * @param boolean $recipe
+     */
+    public function setRecipe($recipe)
+    {
+        $this->recipe = $recipe;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIcon()
+    {
+        return $this->icon;
+    }
+
+    /**
+     * @param string $icon
+     */
+    public function setIcon($icon)
+    {
+        $this->icon = $icon;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIngredient()
+    {
+        return $this->ingredient;
+    }
+
+    /**
+     * @param string $ingredient
+     */
+    public function setIngredient($ingredient)
+    {
+        $this->ingredient = $ingredient;
+    }
+}
---
+++ typo3conf/ext/angelshop/Configuration/TCA/Overrides/tx_news_domain_model_news.php
@@ -0,0 +1,64 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *  (c) 29.07.2016 Michael <mi.blunck@gmail.com>
+ *  All rights reserved
+ *  This is a part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ *  Created by PhpStorm.
+ ******************************************************************/
+
+if (!defined('TYPO3_MODE')) {
+    die ('Access denied.');
+}
+
+
+$newsColumns = [
+    'tx_angelshop_news_recipe' => [
+        'exclude' => 1,
+        'label' => 'LLL:EXT:angelshop/Resources/Private/Language/locallang_db.xlf:news.tx_angelshop_news_recipe',
+        'config' => [
+            'type' => 'check',
+            'default' => '0'
+        ]
+    ],
+    'tx_angelshop_news_ingredient' => [
+        'exclude' => 0,
+        'displayCond' => 'FIELD:tx_angelshop_news_recipe:=:1',
+        'label' => 'LLL:EXT:angelshop/Resources/Private/Language/locallang_db.xlf:news.tx_angelshop_news_ingredient',
+        'config' => array(
+            'type' => 'text',
+            'cols' => 40,
+            'rows' => 6
+        ),
+        'defaultExtras' => 'richtext[]'
+    ],
+    'tx_angelshop_news_icon'                            => [
+        'exclude'     => 1,
+        'label'       => 'LLL:EXT:angelshop/Resources/Private/Language/locallang_db.xlf:news.tx_angelshop_news_icon',
+        'config'      => [
+            'type'  => 'select',
+            'items' => $GLOBALS['TYPO3_CONF_VARS']['FONT_AWESOME'],
+        ],
+    ],
+];
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tx_news_domain_model_news', $newsColumns, 1);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addFieldsToAllPalettesOfField('tx_news_domain_model_news', 'title', 'tx_angelshop_news_recipe', 'before:isTopNews');
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('tx_news_domain_model_news',
+    'tx_angelshop_news_ingredient', '', 'before:bodytext');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('tx_news_domain_model_news',
+    'tx_angelshop_news_icon', '', 'before:tags');
+
+$GLOBALS['TCA']['tx_news_domain_model_news']['ctrl']['requestUpdate'] = 'tx_angelshop_news_recipe';
