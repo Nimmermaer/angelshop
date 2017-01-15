9da8a018a641df146a10db0c0b25dc5906a58ff4
Michael Blunck <michael.blunck@phth.de>
2017-01-16 00:59:11 +0100

    [BUGFIX] Fixes accordion uid

--- typo3conf/ext/angelshop/Resources/Private/Templates/Gridelements/accordion/accordion.ts
+++ typo3conf/ext/angelshop/Resources/Private/Templates/Gridelements/accordion/accordion.ts
@@ -2,7 +2,8 @@ tt_content.gridelements_pi1.20.10.setup.accordion {
     columns {
         20 {
             renderObj =< tt_content
-            wrap = <div class="panel-group" id="accordion-{field:uid}">|</div>
+            dataWrap = <div class="panel-group" id="accordion-{field:uid}">|</div>
+            insertData=1
         }
     }
 
