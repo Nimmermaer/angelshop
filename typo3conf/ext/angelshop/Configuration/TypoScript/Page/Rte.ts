a698ccac5ccdcdddccc70d1c92defa714060c6c7
Michael Blunck <michael.blunck@phth.de>
2017-01-18 00:56:47 +0100

    [TASK] Configures rte

--- typo3conf/ext/angelshop/Configuration/TypoScript/Page/Rte.ts
+++ typo3conf/ext/angelshop/Configuration/TypoScript/Page/Rte.ts
@@ -1,3 +1,26 @@
-RTE.default {
-    hideButtons = about
+RTE {
+    classes := removeFromList(csc-frame-frame1, csc-frame-frame2l)
+    default {
+        proc.allowedClasses := removeFromList(csc-frame-frame1, csc-frame-frame2)
+        hideButtons = about, table,textstyle, textindicator,subscript, superscript
+        showButtons := addToList(underline)
+        buttons {
+            formatblock {
+                removeItems =  h5, h6,pre, address, article, aside, blockquote, div, footer, header, nav, section, p
+
+            }
+            blockstyle {
+                tags {
+                    all {
+                        allowedClasses := removeFromList(csc-frame-frame1, csc-frame-frame2)
+                    }
+                    p {
+                        allowedClasses := removeFromList(csc-frame-frame1, csc-frame-frame2)
+                    }
+                    showTagFreeClasse = 1
+                    postfixLabelWithClassName = 1
+                }
+            }
+        }
+    }
 }
