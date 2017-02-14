979ddfca055ac7c05daa61285f7c3aa37a87da2e
Michael Blunck <michael.blunck@phth.de>
2017-02-14 23:14:54 +0100

    [TASK] Adds content ids

--- typo3conf/ext/angelshop/Configuration/TypoScript/Constants/module.constants.ts
+++ typo3conf/ext/angelshop/Configuration/TypoScript/Constants/module.constants.ts
@@ -1,15 +1,15 @@
 module.tx_angelshop_productlist {
     view {
-        # cat=module.tx_themewhite_customizer/file; type=string; label=Path to template root (BE)
+        # cat=module.tx_angelshop_productlist/file; type=string; label=Path to template root (BE)
         templateRootPath = EXT:angelshop/Resources/Private/Backend/Templates/
-        # cat=module.tx_themewhite_customizer/file; type=string; label=Path to template partials (BE)
+        # cat=module.tx_angelshop_productlist/file; type=string; label=Path to template partials (BE)
         partialRootPath = EXT:angelshop/Resources/Private/Backend/Partials/
-        # cat=module.tx_themewhite_customizer/file; type=string; label=Path to template layouts (BE)
+        # cat=module.tx_angelshop_productlist/file; type=string; label=Path to template layouts (BE)
         layoutRootPath = EXT:angelshop/Resources/Private/Backend/Layouts/
     }
 
     persistence {
-        # cat=module.tx_themewhite_customizer//a; type=string; label=Default storage PID
+        # cat=module.tx_angelshop_productlist//a; type=string; label=Default storage PID
         storagePid =
     }
 }
