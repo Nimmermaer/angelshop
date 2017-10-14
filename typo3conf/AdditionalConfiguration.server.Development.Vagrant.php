9ff52577e6d7eb1639ddbdeafe478db264489c32
Michael Blunck <michael.blunck@phth.de>
2017-10-15 01:43:08 +0200

    [TASK] Extends vagrant configuration

--- typo3conf/AdditionalConfiguration.server.Development.Vagrant.php
+++ typo3conf/AdditionalConfiguration.server.Development.Vagrant.php
@@ -3,6 +3,11 @@ $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['host'] = 'localhost
 $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['dbname'] = 'vagrant';
 $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['user'] = 'vagrant';
 $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['password'] = 'vagrant';
+
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['displayErrors'] = 1;
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['enableDeprecationLog'] ='devlog';
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['syslogErrorReporting'] = '30711';
+
 $GLOBALS['TYPO3_CONF_VARS']['BE']['installToolPassword'] = '$P$CfQEMnMs/X08N4vG7Cqgyb/aMdRvB91';
 $GLOBALS['TYPO3_CONF_VARS']['GFX']['im_version_5'] = 'im6';
 $GLOBALS['TYPO3_CONF_VARS']['GFX']['im_path'] = '/usr/bin/';
