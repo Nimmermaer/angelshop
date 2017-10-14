89a7eef9e2646fb251714912eebd57ccc862c28c
Michael Blunck <michael.blunck@phth.de>
2017-10-15 01:23:02 +0200

    [TASK] Adds AdditionalConfiguration for live

---
+++ typo3conf/AdditionalConfiguration.server.Production.Live.php
@@ -0,0 +1,7 @@
+<?php
+$GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['host'] = 'db638756891.db.1and1.com';
+$GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['dbname'] = 'db638756891';
+$GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['user'] = 'dbo638756891';
+$GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['password'] = 'Srm4p8RJQEBW';
+
+
