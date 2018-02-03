00505fee8329e3b5437a981ad06be03b18f4a71b
Michael Blunck <michael.blunck@phth.de>
2018-02-03 13:18:08 +0100

    [TASK] Prepared angelshop for typo3 v9
    moves package in webroot

---
+++ web/typo3conf/ext/angelshop/Configuration/TypoScript/Page/mod.ts
@@ -0,0 +1,31 @@
+mod {
+    SHARED {
+        defaultLanguageFlag = de
+        defaultLanguageLabel = Deutsch
+    }
+}
+
+
+        mod.web_list {
+            listOnlyInSingleTableView = 1
+        }
+
+
+mod.web_func.menu.function {
+      TYPO3\CMS\WizardSortpages\View\SortPagesWizardModuleFunction = 0
+   }
+
+
+   setup {
+       startModule = web_layout
+       default {
+           emailMeAtLogin = 1
+       }
+       fields {
+
+           avatar.disabled = 1
+       }
+   }
+
+
+
