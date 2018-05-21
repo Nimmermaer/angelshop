bf9640efcd8bc18706a270aa921650a8d7cebf52
Michael Blunck <michael.blunck@phth.de>
2018-05-21 17:16:23 +0200

    [TASK] Adds vagrant configuration

---
+++ web/typo3conf/AdditionalConfiguration.local.Development.Vagrant.php
@@ -0,0 +1,11 @@
+<?php
+$GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['dbname'] = 'vagrant';
+$GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['host'] = 'localhost';
+$GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['password'] = 'vagrant';
+$GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['user'] = 'vagrant';
+
+$GLOBALS['TYPO3_CONF_VARS']['BE']['installToolPassword'] =  '$P$C5SZFI1NA.uXTAnkdq14a8ioCwKHaD.';
+
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['displayErrors'] = 1;
+
+
